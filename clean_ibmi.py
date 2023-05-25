9946ca10fffeccd306e94e3fbe7c48759a5cf0ae
Ihovanna Huezo <ihovannis@gmail.com>
2023-05-25 12:18:19 -0700

    Created function to define correct url paths depending on operation to be performed

--- clean_ibmi.py
+++ clean_ibmi.py
@@ -11,14 +11,14 @@ Please gather and fill in all your information prior to running your script.
 '''
 user_account = 'account@skytap.com' # Skytap user account
 API_key = '0000000' # API key or user account password
-exr_key = '0000000' # ExpressRoute service key
-exr_region = '' # (?) same as all Skytap's envs' regions????? ****
 env_region = '' # Insert region name of your landing zone
 env_template = '0000000' # Insert region-based environment template ID
 env_name = 'Sample Name' # Assign preferred name to your new environment
 vm_template = '0000000' # Insert region-based VM template ID
 env_subnet = '10.0.0.0/24' # Define network subnet address range
 env_gateway = '10.0.0.254' # Define network gateway IPv4 address
+exr_key = '0000000' # Azure ExpressRoute service key
+exr_region = '' # (?) same as Skytap's envs' regions????? ****
 
 
 ## Constants
@@ -30,3 +30,39 @@ Do not modify them.
 url = 'https://cloud.skytap.com/'
 auth = (user_account, API_key)
 headers = { 'Accept': 'application/json', 'Content-type': 'application/json'}
+
+def url(type, env_id=''):
+    # Create new environments
+    if type == 'configurations':
+        return url + 'configurations.json'
+
+    # Create new LPARs or VMs
+    # Modify network configuration
+    elif type == 'environment':
+        return url + f'configurations/{env_id}.json'
+
+    # Generate public IP address
+    elif type == 'ip_address':
+        return url + 'ips/acquire.json'
+
+    # New WAN/Azure ExpressRoute
+    elif type == 'wan':
+        return url + 'vpns.json'
+    
+    else:
+        raise('Must specify type of operation to continue.')
+
+'''
+'https://cloud.skytap.com/configurations.json' --> all environments
+    - Create an environment
+
+'https://cloud.skytap.com/configurations/{env_id}.json' --> within an env
+    - Create new LPARs and VMs
+    - Configure network
+
+'https://cloud.skytap.com/ips/acquire.json' --> IP addresses
+    - Generate an public IP address (???)
+
+'https://cloud.skytap.com/vpns.json' --> WAN resources
+    - Create new ExpressRoute
+'''
