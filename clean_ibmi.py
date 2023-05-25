a611683f6e312772dfaec96341bfecd2396be59e
Ihovanna Huezo <ihovannis@gmail.com>
2023-05-25 11:23:32 -0700

    Defined all variables needed from the user prior to running script

--- clean_ibmi.py
+++ clean_ibmi.py
@@ -10,7 +10,15 @@ Please gather and fill in all your information prior to running your script.
 ** input all as strings ** 
 '''
 user_account = 'account@skytap.com' # Skytap user account
-API_key = 'numbers' # API key or user accoutn password
+API_key = '0000000' # API key or user account password
+exr_key = '0000000' # ExpressRoute service key
+exr_region = '' # (?) same as all Skytap's envs' regions????? ****
+env_region = '' # Insert region name of your landing zone
+env_template = '0000000' # Insert region-based environment template ID
+env_name = 'Sample Name' # Assign preferred name to your new environment
+vm_template = '0000000' # Insert region-based VM template ID
+env_subnet = '10.0.0.0/24' # Define network subnet address range
+env_gateway = '10.0.0.254' # Define network gateway IPv4 address
 
 
 ## Constants
