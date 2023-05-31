a724adb48e734046ffba5c4d5c45dee55808c101
Ihovanna Huezo <ihovannis@gmail.com>
2023-05-31 08:24:08 -0700

    Changed empty strings in User's variables to a generic example.

--- clean_ibmi.py
+++ clean_ibmi.py
@@ -10,13 +10,13 @@ Please gather and fill in all your information prior to running your script.
 '''
 user_account = 'account@skytap.com' # Skytap user account
 API_key = '0000000' # API key or user account password
-env_region = '' # Insert region name of your landing zone (see README)
+env_region = 'Sample-Region' # Insert region name of your landing zone (see README)
 env_template = '0000000' # Insert region-based environment template ID
 env_name = 'Sample Name' # Assign preferred name to your new environment
 vm_template = '0000000' # Insert region-based VM template ID
 env_subnet = '10.0.0.0/24' # Define network subnet address range
 env_gateway = '10.0.0.254' # Define network gateway IPv4 address
-exr_name = '' # Assign preferred name to ExpressRoute circuit
+exr_name = 'Sample Name' # Assign preferred name to ExpressRoute circuit
 exr_key = '0000000' # Azure ExpressRoute service key
 
 
